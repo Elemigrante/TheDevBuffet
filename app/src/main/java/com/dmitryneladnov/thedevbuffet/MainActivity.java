d19831802de830f3c9a5fcb04f06dff3a0cb3241
Neladnov Dmitry <gasdom@yandex.ru>
2021-11-11 22:20:05 +0300

    Initial commit

---
+++ app/src/main/java/com/dmitryneladnov/thedevbuffet/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dmitryneladnov.thedevbuffet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_main);
+    }
+}
