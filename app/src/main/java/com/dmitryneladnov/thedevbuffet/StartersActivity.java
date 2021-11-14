d81c6fddead42847a9d6faef0a6b86ff1e12a9dc
Neladnov Dmitry <gasdom@yandex.ru>
2021-11-14 09:55:57 +0300

    feat: create StarterDishes array

--- app/src/main/java/com/dmitryneladnov/thedevbuffet/StartersActivity.java
+++ app/src/main/java/com/dmitryneladnov/thedevbuffet/StartersActivity.java
@@ -13,5 +13,29 @@ public class StartersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starters);
 
         ListView startersList = findViewById(R.id.list_view_starters);
+
+        String[] starterDishes = {
+                "Spinach and cheese wontons",
+                "Spinach and pesto gyoza",
+                "Thyme and pesto soup",
+                "Chilli and persimmon dumplings",
+                "Parsley and squid soup",
+                "Pineapple and orange soup",
+                "Ricotta and pheasant gyoza",
+                "Pancetta and cannellini soup",
+                "Cheese and squash gyoza",
+                "Stilton and trout soup",
+                "Trout and leek parcels",
+                "Rabbit and beetroot soup",
+                "Ricotta and horseradish wontons",
+                "Cabbage and sausage soup",
+                "Garam masala and pigeon parcels",
+                "Cardamom and fennel soup",
+                "Leek and chicken wontons",
+                "Trout and sausage wontons",
+                "Peppercorn and crab gyoza",
+                "Aubergine and salmon uramaki"
+        };
+
     }
 }
