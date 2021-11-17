5e2fecbf53f628f727a24e205b899d41197e3546
Neladnov Dmitry <gasdom@yandex.ru>
2021-11-17 21:11:21 +0300

    feat: added DessertsActivity

---
+++ app/src/main/java/com/dmitryneladnov/thedevbuffet/DesertsActivity.java
@@ -0,0 +1,14 @@
+package com.dmitryneladnov.thedevbuffet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesertsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deserts);
+    }
+}
--- app/src/main/java/com/dmitryneladnov/thedevbuffet/MainActivity.java
+++ app/src/main/java/com/dmitryneladnov/thedevbuffet/MainActivity.java
@@ -13,13 +13,15 @@ public class MainActivity extends AppCompatActivity {
 
     CardView startersCard;
     CardView mainsCard;
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
-        mainsCard = findViewById(R.id.card_view_main);
+        mainsCard = findViewById(R.id.card_view_main_courses);
+        dessertsCard = findViewById(R.id.card_view_deserts);
 
         startersCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mainCoursesActivityIntent);
             }
         });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dessertsActivityIntent = new Intent(MainActivity.this, DesertsActivity.class);
+                startActivity(dessertsActivityIntent);
+            }
+        });
+
 
         TextView emailTextView = findViewById(R.id.text_view_email_address);
         emailTextView.setOnClickListener(new View.OnClickListener() {
